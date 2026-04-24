5472f282027d96d0bd39a7202b95336c7487a8ff
Anudeepthi <anudeepthikolagani.999@gmail.com>
2026-04-24 11:51:13 +0530

    Styled the UI with Tailwind CSS

--- src/components/Products.js
+++ src/components/Products.js
@@ -10,15 +10,18 @@ const Products = () => {
   const { title, brand, price, rating, description, images } = productDetails;
 
   return (
-    <div className="product-details">
-      <h2>Products</h2>
-      <h4>Product id: {productId}</h4>
-      <img src={images?.[0]} alt={title} className="cardImage" />
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <p>Brand: {brand}</p>
-      <p>Price: ${price}</p>
-      <h4>Rating: {rating} stars</h4>
+    <div className="product-details  w-[700px] mx-auto flex flex-col items-center justify-center gap-4 p-4">
+      <div className="w-72 h-72">
+        <img src={images?.[0]} alt={title} className="cardImage object-cover" />
+      </div>
+      <div className="flex gap-4 justify-between items-center ">
+        <h3 className="font-bold text-lg">{title}</h3>
+        <p className="text-sm font-bold">${price}</p>
+      </div>
+      <p className="font-semibold">Brand: {brand}</p>
+      <h4 className="font-bold text-sm">Rating: {rating} stars</h4>
+
+      <p className="text-gray-600">{description}</p>
     </div>
   );
 };
